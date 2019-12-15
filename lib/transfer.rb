14a94c27dddfb2d3db40bf94bdb2ada09417deee
Calvin McDowell <calvinmac40@hotmail.com>
2019-12-15 22:58:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,39 +1,39 @@
 require 'pry'
 
-# class Transfer
-#   attr_accessor :sender, :receiver, :transfer, :status, :amount, :valid
+class Transfer
+  attr_accessor :sender, :receiver, :transfer, :status, :amount, :valid
   
-#   def initialize(sender, receiver, transfer)
-#     @sender = sender
-#     @receiver = receiver
-#     @transfer = transfer
-#     @status = "pending"
-#     @amount = 50
-#   end
+  def initialize(sender, receiver, transfer)
+    @sender = sender
+    @receiver = receiver
+    @transfer = transfer
+    @status = "pending"
+    @amount = 50
+  end
   
-#   def valid?
-#     @sender.valid? && @receiver.valid?
-#   end
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
   
-#   def execute_transaction
-#     if  @sender.valid? && @sender.balance > @amount && @status == "pending"
-#         @sender.balance -= @amount
-#         @receiver.balance += @amount
-#         @status = "complete"
-#     else
-#       @status = "rejected"
-#       "Transaction rejected. Please check your account balance."
-#     end
-#   end
+  def execute_transaction
+    if  @sender.valid? && @sender.balance > @amount && @status == "pending"
+        @sender.balance -= @amount
+        @receiver.balance += @amount
+        @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
   
-#   def reverse_transfer
-#     if @status == "rejected"
-#       @receiver.balance -= @amount
-#       @sender.balance += @amount
-#       @status = "reversed"
-#     end
-#   end
-# end
+  def reverse_transfer
+    if @status == "rejected"
+      @receiver.balance -= @amount
+      @sender.balance += @amount
+      @status = "reversed"
+    end
+  end
+end
 
 class Transfer
   attr_accessor :sender, :receiver, :amount, :status

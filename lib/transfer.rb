69fe633bfc6a9cdab05d125a3455ec561a62b807
Calvin McDowell <calvinmac40@hotmail.com>
2019-12-15 23:38:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,10 +20,6 @@ class Transfer
         @sender.balance -= @amount
         @receiver.balance += @amount
         @status = "complete"
-        #binding.pry
-    else
-      @status = "rejected"
-      "Transaction rejected. Please check your account balance."
     end
   end
   

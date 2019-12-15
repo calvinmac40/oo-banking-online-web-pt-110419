a1adff7fcd4ec89b0e2ce59e5f69345f52bb9dc1
Calvin McDowell <calvinmac40@hotmail.com>
2019-12-15 22:54:35 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,10 +16,10 @@ class Transfer
   end
   
   def execute_transaction
-    if valid? && @sender.balance > @amount && @status == "pending"
-      @sender.balance -= @amount
-      @receiver.balance += @amount
-      @status = "complete"
+      if@sender.valid? && @sender.balance > @amount && @status == "pending"
+        @sender.balance -= @amount
+        @receiver.balance += @amount
+        @status = "complete"
     else
       @status = "rejected"
       "Transaction rejected. Please check your account balance."

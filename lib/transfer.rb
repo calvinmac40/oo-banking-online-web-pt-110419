fc815f543436633f798633369ee284953005d413
Calvin McDowell <calvinmac40@hotmail.com>
2019-12-15 23:43:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   end
   
   def execute_transaction
-    if  @sender.balance > @amount
+    if  valid? && @sender.balance > @amount && @status == "pending"
         @sender.balance -= @amount
         @receiver.balance += @amount
         @status = "complete"

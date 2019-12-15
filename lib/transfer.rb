19f6415fed78def75834950647c659d51beaeb5c
Calvin McDowell <calvinmac40@hotmail.com>
2019-12-15 22:56:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,86 +1,86 @@
 require 'pry'
 
-class Transfer
-  attr_accessor :sender, :receiver, :transfer, :status, :amount, :valid
-  
-  def initialize(sender, receiver, transfer)
-    @sender = sender
-    @receiver = receiver
-    @transfer = transfer
-    @status = "pending"
-    @amount = 50
-  end
-  
-  def valid?
-    @sender.valid? && @receiver.valid?
-  end
-  
-  def execute_transaction
-    if  @sender.valid? && @sender.balance > @amount && @status == "pending"
-        @sender.balance -= @amount
-        @receiver.balance += @amount
-        @status = "complete"
-    else
-      @status = "rejected"
-      "Transaction rejected. Please check your account balance."
-    end
-  end
-  
-  def reverse_transfer
-    if @status == "rejected"
-      @receiver.balance -= @amount
-      @sender.balance += @amount
-      @status = "reversed"
-    end
-  end
-end
-
 # class Transfer
-#   attr_accessor :sender, :receiver, :amount, :status
-
-#   def initialize(sender, receiver, amount)
+#   attr_accessor :sender, :receiver, :transfer, :status, :amount, :valid
+  
+#   def initialize(sender, receiver, transfer)
 #     @sender = sender
 #     @receiver = receiver
-#     @amount = amount
+#     @transfer = transfer
 #     @status = "pending"
+#     @amount = 50
 #   end
-
+  
 #   def valid?
-#     @sender.valid? && @receiver.valid? ? true : false
+#     @sender.valid? && @receiver.valid?
 #   end
-
-#   # def execute_transaction
-#   #   if @sender.balance > @amount || @status == "pending"
-#   #       @sender.balance - @amount
-#   #       @receiver.balance + @amount
-#   #       @status = "complete"
-#   #   elsif @sender != valid?
-#   #       @status = "rejected"
-#   #     "Transaction rejected. Please check your account balance."
-#   #   end
-#   #   end
   
 #   def execute_transaction
-#     if  @sender.balance != valid?
+#     if  @sender.valid? && @sender.balance > @amount && @status == "pending"
+#         @sender.balance -= @amount
+#         @receiver.balance += @amount
+#         @status = "complete"
+#     else
 #       @status = "rejected"
-#       return "Transaction rejected. Please check your account balance."
-#     else @sender.balance > @amount && @status == "pending"
-#       @sender.balance -= @amount
-#       @receiver.balance += @amount
-#       @status = "complete"
-#       #binding.pry
+#       "Transaction rejected. Please check your account balance."
 #     end
 #   end
-
-   
+  
 #   def reverse_transfer
-#     if @status == "complete"
-#       @sender.balance += @amount
+#     if @status == "rejected"
 #       @receiver.balance -= @amount
+#       @sender.balance += @amount
 #       @status = "reversed"
 #     end
 #   end
 # end
+
+class Transfer
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid? ? true : false
+  end
+
+  # def execute_transaction
+  #   if @sender.balance > @amount || @status == "pending"
+  #       @sender.balance - @amount
+  #       @receiver.balance + @amount
+  #       @status = "complete"
+  #   elsif @sender != valid?
+  #       @status = "rejected"
+  #     "Transaction rejected. Please check your account balance."
+  #   end
+  #   end
+  
+  def execute_transaction
+    if  @sender.balance != valid?
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    else @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+      #binding.pry
+    end
+  end
+
+   
+  def reverse_transfer
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end
+  end
+end
 
 # instead of if @sender.balance > @amount && @status == "pending"
 # User avatar
